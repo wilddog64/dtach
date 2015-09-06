8ea1e3ddb4861f6ea6b737bdd6248ca9dc3595c2
chengkai liang <ckm.liang@gmail.com>
2015-09-06 11:35:05 -0700

    add git cookbook 4.3.3

--- Berksfile
+++ Berksfile
@@ -1,3 +1,4 @@
 source 'https://supermarket.chef.io'
 
 metadata
+cookbook 'git', '4.3.3', 'jssjr/git'
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures dtach'
 long_description 'Installs/Configures dtach'
 version '0.1.0'
+
+depends 'git', '<= 4.3.3'
