cb60920f1a3cab3a6f5f6ed41cdfb73451a5917a
chengkai liang <ckm.liang@gmail.com>
2015-09-07 09:04:16 -0700

    check binary is built

--- files/default/test/deploy_test.rb
+++ files/default/test/deploy_test.rb
@@ -8,4 +8,12 @@ describe_recipe 'deploy' do
     app_path         = ::File.join(file_cache_path, app_name)
     directory(app_path).must_exist.with(:user, 'root').and(:group, 'root').and(:mode, '0755')
   end
+
+  it 'builds dtach binary' do
+    file_cache_path = Chef::Config['file_cache_path']
+    app_name        = node['dtach']['git']['app']
+    app_path        = ::File.join(file_cache_path, app_name)
+    dtach_binary    = ::File.join(app_path, 'dtach')
+    file(dtach_binary).must_exist.with(:mode, '0755').and(:group, 'root').and(:user, 'root')
+  end
 end
