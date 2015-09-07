6435102948c33f031ec5ca70f65909eb5c1574de
chengkai liang <ckm.liang@gmail.com>
2015-09-07 09:08:06 -0700

    bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures dtach'
 long_description 'Installs/Configures dtach'
-version '0.1.0'
+version '0.2.0'
 
 depends 'git', '<= 4.3.3'
 depends 'minitest-handler', '<= 1.4.0'
